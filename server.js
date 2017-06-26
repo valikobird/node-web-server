e7e6046a2af238f5f5c252a6ef3c9134eb4e1cf1
Valentyn Zozulia <valikobird@gmail.com>
2017-06-26 13:51:46 -0400

    Add static files serving

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express');
 
 var app = express();
 
+app.use(express.static(__dirname + '/public'));
+
 app.get('/', (req, res) => {
     res.send({
         name: 'Valiko',
@@ -23,4 +25,6 @@ app.get('/bad', (req, res) => {
     });
 });
 
-app.listen(3000);
+app.listen(3000, () => {
+    console.log('Server is up on port 3000');
+});
